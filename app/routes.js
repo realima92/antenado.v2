d29a436a66f43cfc89438601c8a288e689b17a15
Renato Lima <renato.lima92@gmail.com>
2018-07-02 15:53:36 -0300

    Aulas de Node.js

--- app/routes.js
+++ app/routes.js
@@ -2,19 +2,21 @@ module.exports = function(app){
     app.get("/", function(req,res){
         var mysql = require('mysql');
         var connection = mysql.createConnection({
-            host: 'localhost',
+            host: '127.0.0.1',
             user: 'root',
-            password: '',
-            database: 'antenadoDB'
+            port: '3306',
+            password: ''
         });
         //consulta
-        
-        connection.query('select * from livros', function(err, results){
-            res.send(results);
+        connection.connect(function(err){
+            if(err) throw err;
+            console.log("Banco de dados conectado")
+        });
+
+        connection.query('SELECT * FROM nodeschema.livros', function(err, results){
+            res.render('index', {lista:results});
         });
 
         connection.end();
-
-        res.render("index");
     });
 }
--- config/express.js
+++ config/express.js
@@ -2,6 +2,6 @@ module.exports = function(){
     console.log("Servidor sendo carregado...")
     var app = require('express')();
     app.set('view engine', 'ejs');
-    app.set('views','./app/views')
+    app.set('views','./app/views');
     return app;
 }
